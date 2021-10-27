6997682b61469404c6be2c49986e8885798b895c
ofthegoats <47647338+ofthegoats@users.noreply.github.com>
2021-10-27 20:14:35 +0100

    feat(tokenization): types and tokenization function
    Added the Token, OP types, reusable definitions for common operations,
    and made a tokenization function.
    
    I do not currently believe there is much I can change about the
    tokenization function, at least behaviourally.
    
    The implementation of Token is not currently satisfying, however I am
    currently tired and have no ideas.

--- main.hs
+++ main.hs
@@ -1,2 +1,40 @@
+import Data.Char (isAlphaNum)
 import Structures.Queue
 import Structures.Stack
+import Text.Read (readMaybe)
+
+data OP = Power | Modulo | Times | Divide | Plus | Minus deriving (Show, Eq)
+
+data Token
+  = Operator OP Int
+  | Variable String
+  | Number Double
+  | OpenParen
+  | CloseParen
+  deriving (Show, Eq)
+
+power, modulo, times, divide, plus, minus :: Token
+power = Operator Power 2
+modulo = Operator Modulo 3
+times = Operator Times 3
+divide = Operator Divide 3
+plus = Operator Plus 4
+minus = Operator Minus 4
+
+tokenize :: String -> [Token]
+tokenize [] = []
+tokenize (' ' : xs) = tokenize xs -- ignore spaces
+tokenize ('(' : xs) = OpenParen : tokenize xs
+tokenize (')' : xs) = CloseParen : tokenize xs
+tokenize ('^' : xs) = power : tokenize xs
+tokenize ('%' : xs) = modulo : tokenize xs
+tokenize ('*' : xs) = times : tokenize xs
+tokenize ('/' : xs) = divide : tokenize xs
+tokenize ('+' : xs) = plus : tokenize xs
+tokenize ('-' : xs) = minus : tokenize xs
+tokenize xs =
+  let term = takeWhile (\x -> isAlphaNum x || x == '.') xs
+      rest = drop (length term) xs
+   in case readMaybe term :: Maybe Double of
+        Nothing -> Variable term : tokenize rest
+        Just d -> Number d : tokenize rest
